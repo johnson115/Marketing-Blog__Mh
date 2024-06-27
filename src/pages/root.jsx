ee802b707f0e697d9927585c074005d8227f5529
johnson <amencabnaamen@gmail.com>
2024-06-27 16:41:49 +0100

    Day one

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -5,12 +5,14 @@
 import React from 'react';
 import Hero from './hero/hero';
 import Navbar from './navbar/navbar';
+import Service from './service/service';
 
 const Root = () => {
   return (
     <>
     <Navbar />
 <Hero />
+<Service />
     </>
   );
 }
